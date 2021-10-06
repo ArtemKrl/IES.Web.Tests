3e95ef306bcd8d2416484682dd6e4a4d7240e4e2
daz <frozendevs@hotmail.com>
2021-10-06 15:12:04 +0300

    first commit

---
+++ pulldrivers.sh
@@ -0,0 +1,5 @@
+#/!bin/bash
+
+docker pull selenoid/chrome:89.0
+docker pull selenoid/firefox:88.0
+#docker pull selenoid/opera:76.0
---
+++ tests/capabilities.py
@@ -0,0 +1,8 @@
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "89.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": True
+    }
+}
