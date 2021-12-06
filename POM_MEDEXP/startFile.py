f483bf5a750c947e801cf71fc4c943d3ded06d49
Artem.Korol <artsab9@gmail.com>
2021-12-06 15:00:34 +0300

    Файл старта всех тестов

---
+++ POM_MEDEXP/startFile.py
@@ -0,0 +1,13 @@
+import pytest
+from POM_MEDEXP.Tests.test_HomePage import *
+from POM_MEDEXP.Tests.test_LoginPage import *
+
+
+def test_LoginPage():
+    pass
+
+
+def test_HomePage():
+    pass
+
+
