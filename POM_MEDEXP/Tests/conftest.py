470e81d6b91ca803f34d99193e5eb553b552911d
Artem.Korol <artsab9@gmail.com>
2021-12-06 15:27:11 +0300

    подключён селеноид

--- POM_MEDEXP/Tests/conftest.py
+++ POM_MEDEXP/Tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from Config.config import TestData
+from POM_MEDEXP.Config.config import TestData
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from POM_MEDEXP.Config.capabilities import capabilities
@@ -10,7 +10,8 @@ from POM_MEDEXP.Config.config import selenoid_URL
 @pytest.fixture(params=["chrome"], scope='class')
 def init_driver(request):
     if request.param == "chrome":
-        web_driver = WebDriver()
+        web_driver = webdriver.Remote(command_executor=selenoid_URL,
+                                      desired_capabilities=capabilities)
     if request.param == "firefox":
         web_driver = webdriver.Firefox(executable_path=TestData.FIREFOX_EXECUTABLE_PATH)
     request.cls.driver = web_driver
