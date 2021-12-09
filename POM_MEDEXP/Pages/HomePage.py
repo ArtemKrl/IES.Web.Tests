98f973c5450afd8cc0321dd5705e90ae7f255390
Artem.Korol <artsab9@gmail.com>
2021-12-09 22:22:46 +0300

    добавлен тест открытия DataFormSandbox

--- POM_MEDEXP/Pages/HomePage.py
+++ POM_MEDEXP/Pages/HomePage.py
@@ -1,25 +1,34 @@
+import time
+
 from POM_MEDEXP.Pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class HomePage(BasePage):
 
-    GENERA_ADM = (By.CSS_SELECTOR, '.anticon-right-square.ant-menu-item-icon')
-    ADDITIONAL_DFS = (By.CSS_SELECTOR, '.ant-layout-sider-light.ant-layout-sider-has-trigger > '
-                                        'div.ant-layout-sider-children > ul > li:nth-child(3)')
+    GENERAL_ADM = (By.CSS_SELECTOR, '.anticon-right-square.ant-menu-item-icon')
+    ADDITIONAL_DFS = (By.CSS_SELECTOR, '.ant-menu-light > li:nth-child(2) > span:nth-child(1)')
+    DFS_PAGE = (By.CSS_SELECTOR, ".appbar-div > div > div:nth-child(2) > div")
     VITACORE_LOGO = (By.CSS_SELECTOR, ".NavRail_logo__2mva4 > svg")
 
 
     def __init__(self, driver):
-        self().__init__(driver)
+        super().__init__(driver)
+
+
 
     def is_vitacore_logo_exist(self):
         return self.is_visible(self.VITACORE_LOGO)
 
-    def elect_admin(self):
-        if self.is_visible(self.GENERA_ADM):
-            return self.do_click(self.GENERA_ADM)
-
-    def elect_admin(self):
+    def elect_DFS(self):
+        if self.is_visible(self.GENERAL_ADM):
+            self.do_click(self.GENERAL_ADM)
         if self.is_visible(self.ADDITIONAL_DFS):
-            return self.do_click(self.ADDITIONAL_DFS)
+            self.do_click(self.ADDITIONAL_DFS)
+
+    def DFS_page_is_open(self, text):
+        return self.get_element_text(self.DFS_PAGE)
+
+
+
--- POM_MEDEXP/Tests/test_HomePage.py
+++ POM_MEDEXP/Tests/test_HomePage.py
@@ -2,12 +2,23 @@ from POM_MEDEXP.Pages.LoginPage import LoginPage
 from POM_MEDEXP.Tests.test_base import BaseTest
 
 from POM_MEDEXP.Config.config import TestData
-from POM_MEDEXP.Pages import HomePage
+from POM_MEDEXP.Pages.HomePage import HomePage
 
 
 class Test_Home(BaseTest):
     def test_home_page_logo(self):
         self.loginPage = LoginPage(self.driver)
         homePage = self.loginPage.do_login(TestData.USER_NAME, TestData.PASSWORD)
-        check_in = self.loginPage.is_vitacore_logo_exist()
+        check_in = homePage.is_vitacore_logo_exist()
         assert check_in
+
+
+    def test_follow_to_DFS(self):
+        self.loginPage = LoginPage(self.driver)
+        homePage = self.loginPage.do_login(TestData.USER_NAME, TestData.PASSWORD)
+        homePage.elect_DFS()
+        page = homePage.DFS_page_is_open(TestData.DFS_TEXT)
+        assert page == TestData.DFS_TEXT
+
+
+
