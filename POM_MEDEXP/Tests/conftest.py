ecdc8c7a6bb2349ed92c29519f34e838d3c05f1d
bakirovaasia <bakirova.asia@gmail.com>
2022-03-03 18:38:51 +0300

    use selenoid

--- POM_MEDEXP/Tests/conftest.py
+++ POM_MEDEXP/Tests/conftest.py
@@ -7,8 +7,8 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 @pytest.fixture(params=["chrome"], scope='class')
 def init_driver(request):
     if request.param == "chrome":
-        web_driver = WebDriver()
-        # web_driver = webdriver.Remote(command_executor=selenoid_URL,desired_capabilities=capabilities)
+        #web_driver = WebDriver()
+        web_driver = webdriver.Remote(command_executor=selenoid_URL,desired_capabilities=capabilities)
     if request.param == "firefox":
         web_driver = webdriver.Firefox(executable_path=TestData.FIREFOX_EXECUTABLE_PATH)
     request.cls.driver = web_driver
