82ee7ae3e6128e3f4c13cc9720361bbf0ef0ade2
Artem M. Korol <Artem M. Korol>
2022-07-20 13:02:08 +0300

    Revert "Удалено: POM_MEDEXP"

---
+++ POM_MEDEXP/businessProcess.py
@@ -0,0 +1,17 @@
+import pytest
+from POM_MEDEXP.Tests.test_OverallPage import *
+
+from POM_MEDEXP.Tests.test_LoginPage import *
+
+def test_OverallPage():
+    pass
+#
+# def test_HomePage():
+#     pass
+#
+# def test_DataForm():
+#     pass
+#
+# def test_ElectionsPage():
+#     pass
+#
